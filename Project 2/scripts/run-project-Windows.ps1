03087458ca49db7f024021de40fd47bbad8de12a
mzmddi <110358757+mzmddi@users.noreply.github.com>
2025-04-08 17:52:52 -0400

    debugged the scripts now it works

--- Project 2/scripts/run-project-Windows.ps1
+++ Project 2/scripts/run-project-Windows.ps1
@@ -1,19 +1,5 @@
 COMPOSE_BAKE=true docker-compose up --build -d
 
-while ($true) {
-    
-    $container = docker ps --filter "name=terminal-io" --filter "status=running"
-    
-    if ($container -match "terminal-io") {
-        Write-Host "Starting Chatbot..."
-        docker attach terminal-io
-        break
-    } else {
-        Write-Host "Waiting all images to be built and containers running..."
-        Start-Sleep -Seconds 10
-    }
-}
+cls 
 
-cls
-
-docker exec -it terminal-io python /terminal_io/terminal_driver.py
+docker start -a terminal-io
--- Project 2/scripts/run-project-macOS.sh
+++ Project 2/scripts/run-project-macOS.sh
@@ -3,25 +3,15 @@
 COMPOSE_BAKE=true docker-compose up --build -d
 # build the project in detach mode
 
-while true; do
-    # checks if the terminal container is running, which is the last container to start
+clear 
+# clears the terminal
 
-    if docker ps --filter "name=terminal-io" --filter "status=running" | grep -q "terminal-io"; then
-        echo "Starting Chatbot..."
-        docker attach terminal-io
-        break
-    else
-        echo "Waiting for docker to build all images and run all containers"
-        sleep 10
-    fi
-done
-
-clear
-
-docker exec -it terminal-io python /terminal_io/terminal_driver.py
+docker start -a terminal-io
+# runs the terminak container in attach mode
 
 
 
+# NOTES:
 # go in the directory of this file
 # do the command ./run-project-macOS.sh
 # this is for macOS
