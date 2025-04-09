839381ec6de341f8909f9790535e7f2935859ce7
mzmddi <110358757+mzmddi@users.noreply.github.com>
2025-04-08 21:03:22 -0400

    main convo loop for user UI interaction

--- Project 2/app/main.py
+++ Project 2/app/main.py
@@ -31,7 +31,12 @@ async def testing(request: Request):
     
     data = await request.json()
     
-    return {"response": data}
+    lorem_ipsum = """
+Lorem ipsum dolor sit amet, consectetur a
+dipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+"""
+    
+    return {"response": lorem_ipsum}
 
 @app.post('/testing/dbpedia/spotlight')
 async def testing(request: Request):
